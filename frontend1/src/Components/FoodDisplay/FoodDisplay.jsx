d69c0d7becae1224674122d3be44a3b9b7623a9a
Priya-Satpathy <priyasatpathy429@gmail.com>
2025-12-21 19:22:46 +0530

    content added

--- frontend1/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend1/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import {StoreContext} from '../../Context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
+import './FoodDisplay.css'
 
 const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext)
--- frontend1/src/Components/FoodItem/FoodItem.jsx
+++ frontend1/src/Components/FoodItem/FoodItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
-
+import './FoodItem.css'
 const FoodItem = ({id,name,price,description,image}) => {
   return (
     <div className='food-item'>
