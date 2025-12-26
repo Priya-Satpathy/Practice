09f1b4c83eeae440a36ad2aa571118376ab71cd9
Priya-Satpathy <priyasatpathy429@gmail.com>
2025-12-26 23:57:51 +0530

    content add

---
+++ frontend1/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div>
+         <p>Lorem Ipsum is simply dummy text of the</p>
+      
+    </div>
+  )
+}
+
+export default Footer
