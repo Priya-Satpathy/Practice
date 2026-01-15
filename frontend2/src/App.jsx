ee8d38ff48183010de235774161423771bf673da
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-01-15 20:43:57 +0530

    content added

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Navbar from './Components/Navbar/Navbar'
+
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <Navbar />
     </div>
   )
 }
