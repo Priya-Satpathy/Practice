b24f694460d7588fa09970e8edb1ef8cb3dc0352
Priya-Satpathy <priyasatpathy429@gmail.com>
2026-01-20 21:24:02 +0530

    content added

--- frontend2/src/Components/FoodItem/FoodItem.jsx
+++ frontend2/src/Components/FoodItem/FoodItem.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
 const FoodItem = ({ id, name, description, price, image }) => {
+  const[itemCount, setItemCount] = useState(0);
   return (
     <div className='food-item'>
       <div className='food-item-img-container'>
         <img className='food-item-image' src={image} alt={name} />
+        {!itemCount
+        ? <img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="add" />
+        : <div className='food-item-count'>
+            <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="remove" />
+            <p>{itemCount}</p>
+            <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="add" />
+          </div>
+        }
+
       </div>
 
       <div className='food-item-info'>
